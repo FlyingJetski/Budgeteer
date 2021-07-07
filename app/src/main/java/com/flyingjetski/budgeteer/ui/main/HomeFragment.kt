7b65ccef0e1798cab57edd509a7bee526cbba173
FlyingJetski <joshuaxu71@gmail.com>
2021-07-07 21:58:32 +0700

    introduced piechart

--- app/src/main/java/com/flyingjetski/budgeteer/ui/main/HomeFragment.kt
+++ app/src/main/java/com/flyingjetski/budgeteer/ui/main/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.flyingjetski.budgeteer.ui.main
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 import com.flyingjetski.budgeteer.R
 import com.flyingjetski.budgeteer.databinding.FragmentHomeBinding
 import com.flyingjetski.budgeteer.databinding.FragmentStatisticsBinding
+import org.eazegraph.lib.models.PieModel
 
 class HomeFragment : Fragment() {
 
@@ -21,6 +23,13 @@ class HomeFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        val a = PieModel("Food", 15.0f, Color.parseColor("#36c9b8"))
+        val b = PieModel("Electricity", 7.3f, Color.parseColor("#7543ba"))
+        val c = PieModel("Water", 3.9f, Color.parseColor("#69c74a"))
+        binding.pieChart.addPieSlice(a)
+        binding.pieChart.addPieSlice(b)
+        binding.pieChart.addPieSlice(c)
+
         return binding.root
     }
 
